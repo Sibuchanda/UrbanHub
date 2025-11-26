8b5ed7236416201d6cdb325987ec7da1e12f8345
Sibuchanda <sibuchanda457@gmail.com>
2025-11-26 23:25:30 +0530

    Created controllers for product CRUD operations in backend

--- server/models/Products.js
+++ server/models/Products.js
@@ -12,7 +12,7 @@ const ProductSchema = new mongoose.Schema(
     totalStock: Number,
     averageReview: Number,
   },
-  { timestamps: true }
+ { timestamps: true }
 );
 
 const Product = mongoose.model("Product",ProductSchema);
--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import handleImageUpload from '../../controlers/admin/products-controller.js';
+import handleImageUpload, { addProduct, deleteProduct, editProduct, fetchAllProducts } from '../../controlers/admin/products-controller.js';
 import { upload } from '../../helpers/cloudinary.js';
 
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
-
+router.post("/add", addProduct);
+router.get("/get", fetchAllProducts);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
 
 
 export default router;
